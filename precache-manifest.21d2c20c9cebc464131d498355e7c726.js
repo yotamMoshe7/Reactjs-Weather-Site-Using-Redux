39ce892b95a50e4969851b3fc727f4d50763d274
יותם <yotammoshe7@gmail.com>
2020-10-29 19:26:13 +0200

    Updates

---
+++ precache-manifest.21d2c20c9cebc464131d498355e7c726.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cc3a2388252a03b3d11f79d54f2a5cc",
+    "url": "/weather-site-using-redux/index.html"
+  },
+  {
+    "revision": "236f9fe1a836bf900eb1",
+    "url": "/weather-site-using-redux/static/css/main.f8a7744c.chunk.css"
+  },
+  {
+    "revision": "76f0ceab8ece8832d281",
+    "url": "/weather-site-using-redux/static/js/2.075f8a1d.chunk.js"
+  },
+  {
+    "revision": "bd2ae56b9f4d0dee6711c5bb737f34a3",
+    "url": "/weather-site-using-redux/static/js/2.075f8a1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "236f9fe1a836bf900eb1",
+    "url": "/weather-site-using-redux/static/js/main.b89f9b88.chunk.js"
+  },
+  {
+    "revision": "906142b0fe2410224618",
+    "url": "/weather-site-using-redux/static/js/runtime-main.6f494d5b.js"
+  },
+  {
+    "revision": "08287b8957079216fda11640c0efbf5f",
+    "url": "/weather-site-using-redux/static/media/background4.08287b89.jpg"
+  }
+]);
